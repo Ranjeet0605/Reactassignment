1204986b15e88650e51663997c82e2cf6a2d29b8
Ranjeet <111526289+Ranjeet0605@users.noreply.github.com>
2025-06-22 13:48:49 +0530

    Update Db.js

--- Backend/Db.js
+++ Backend/Db.js
@@ -1,7 +1,5 @@
 const mongoose = require("mongoose");
-const connection = async(req,res)=>{
-    try{
-        await mongoose.connect("mongodb+srv://r6306344:WeIXpjf2Tn0yrDju@cluster0.iucwe.mongodb.net/Instagram").then(()=>{
+const connection = async(req,res)=>{").then(()=>{
             console.log("Database is connected");
         })
     }catch(error){
